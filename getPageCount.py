2095d840dcd64fc0fc15a7d45c157339309349cf
milicjx <milicjx@users.noreply.github.com>
2015-04-20 20:51:23 -0400

    Create getPageCount.py

---
+++ getPageCount.py
@@ -0,0 +1,20 @@
+import re
+import os
+import glob
+
+countpage = re.compile(r"/Type\s*/Page([^s]|$)", re.MULTILINE | re.DOTALL)
+
+def count_pages(filename):
+    data = file(filename, "rb").read()
+    return len(countpage.findall(data))
+
+if __name__ == "__main__":
+    parent = "/Users/username/Desktop"
+    
+    #for infile in glob.glob(os.path.basename('*.pdf')):
+    
+    # replace * extension with pdf, txt, docx, etc.
+    for infile in glob.glob(os.path.join(parent, '*.*')):
+        page_count = str(count_pages(infile))
+        # \t specifies tab space
+        print "".join([infile, "\t", page_count])
