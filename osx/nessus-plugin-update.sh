acaf2a1ddbcbcb5ef0406d55009bb706e991fdee
milicjx <milicjx@users.noreply.github.com>
2015-09-24 23:40:35 -0400

    Update nessus-plugin-update.sh

--- osx/nessus-plugin-update.sh
+++ osx/nessus-plugin-update.sh
@@ -1,7 +1,3 @@
-#!/bin/bash
-echo "updating Nessus plugins"
 cd /Library/Nessus/run/sbin/
 ./Library/Nessus/run/sbin/nessuscli fetch --plugins
 ./Library/Nessus/run/sbin/nessuscli update all-2.0.tar.gz
-echo "exiting"
-exit 0
